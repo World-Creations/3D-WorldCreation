f2b7c2c3f32898764d9b34a67bc280edbd2b94ba
erutled2 <erutled2@stumail.jccc.edu>
2025-12-03 13:16:41 -0600

    Player separated to its own scene - Removed duplicate scenes
    Allows data to be transferred between scenes (By storing data inside the player scene)
    Additionally removes duplicate scenes as they are no longer required for different entrance points
    Entrance points are now defined in-scene and teleport triggers now define a target entrance (by name) defaults to "Default" if no target name is specified
    Scenes require an empty object with the "Entrances" tag; empty child objects will be referenced by name on teleport requests

--- Assets/Scripts/sceneloader.cs
+++ Assets/Scripts/sceneloader.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class sceneloader : MonoBehaviour
 {
-    public string scene;
+    [SerializeField] private string scene;
+    [SerializeField] private string target;
 
     public void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene(scene);
+
+        Debug.Log(other.name);
+        if (target == "" || target == null) target = "Default";
+        if (other.tag == "Player") other.GetComponent<PlayerController>().ChangeScene(scene, target);
     }
 }
